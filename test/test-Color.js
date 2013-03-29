96504fcb08cef6a9b7d3dc45b2c5a16e47231046
Corey Hart <corey@codenothing.com>
2013-03-29 00:32:10 -0700

    Added 'HSL to Hex' Compression

--- test/test-Color.js
+++ test/test-Color.js
@@ -50,6 +50,58 @@ MUnit( 'Color.RGB to Hex', function( assert ) {
 });
 
 
+// HSL to Hex conversions
+MUnit( 'Color.HSL to Hex', function( assert ) {
+	var rule = CSSCompressor._rulesHash[ 'HSL to Hex' ].callback;
+
+	[
+
+		{
+			name: 'Basic',
+			actual: 'hsl(312,54%,68%)',
+			expected: '#d981c8'
+		},
+
+		{
+			name: 'Basic Spacing',
+			actual: 'hsl( 116, 34%, 43% )',
+			expected: '#4d9348'
+		},
+
+		{
+			name: 'No Saturation',
+			actual: 'hsl( 116, 0%, 43% )',
+			expected: '#6e6e6e'
+		},
+
+		{
+			name: 'Invalid Hue',
+			actual: 'hsl( 416, 15%, 43% )',
+			expected: 'hsl( 416, 15%, 43% )'
+		},
+
+		{
+			name: 'Invalid Saturation',
+			actual: 'hsl( 116, 105%, 43% )',
+			expected: 'hsl( 116, 105%, 43% )'
+		},
+
+		{
+			name: 'Invalid Lightness',
+			actual: 'hsl( 116, 10%, 435% )',
+			expected: 'hsl( 116, 10%, 435% )'
+		}
+
+	].forEach(function( object ) {
+		assert.deepEqual(
+			object.name,
+			rule( object.actual, null, compressor ),
+			object.expected
+		);
+	});
+});
+
+
 // Color to Hex conversions
 MUnit( 'Color.Color to Hex', function( assert ) {
 	var rule = CSSCompressor._rulesHash[ 'Color to Hex' ].callback;
