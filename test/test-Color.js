fc76936f194943fe13ecf37d927f2b7d422b67bd
Corey Hart <corey@codenothing.com>
2013-04-01 09:33:01 -0700

    Added 'HSLA to HSL' Compression

--- test/test-Color.js
+++ test/test-Color.js
@@ -4,6 +4,46 @@ var compressor = new CSSCompressor( CSSCompressor.MODE_MAX );
 compressor.log = CSSCompressor.noop;
 
 
+// HSLA to HSL conversions
+munit( 'Color.HSLA to HSL', function( assert ) {
+	var rule = CSSCompressor._rulesHash[ 'HSLA to HSL' ].callback;
+
+	[
+
+		{
+			name: 'Basic',
+			actual: 'hsla(214,10%,10%,1)',
+			expected: 'hsl(214,10%,10%)'
+		},
+
+		{
+			name: 'Basic Spacing',
+			actual: 'hsla( 214, 10%, 10%, 1 )',
+			expected: 'hsl(214,10%,10%)'
+		},
+
+		{
+			name: 'Basic Suffix',
+			actual: 'hsla( 214, 10%, 10%, 1.0 )',
+			expected: 'hsl(214,10%,10%)'
+		},
+
+		{
+			name: 'Not 100%',
+			actual: 'hsla( 214, 10%, 10%, 0.5 )',
+			expected: 'hsla( 214, 10%, 10%, 0.5 )'
+		}
+
+	].forEach(function( object ) {
+		assert.deepEqual(
+			object.name,
+			rule( object.actual, null, compressor ),
+			object.expected
+		);
+	});
+});
+
+
 // RGB to Hex conversions
 munit( 'Color.RGB to Hex', function( assert ) {
 	var rule = CSSCompressor._rulesHash[ 'RGB to Hex' ].callback;
