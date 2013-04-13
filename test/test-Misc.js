338b82ae97f994d7175ed2a1c69681c8e7a36273
Corey Hart <corey@codenothing.com>
2013-04-12 19:51:07 -0700

    -Adding 'Function Units' Compression

--- test/test-Misc.js
+++ test/test-Misc.js
@@ -4,40 +4,52 @@ var compressor = new CSSCompressor( CSSCompressor.MODE_MAX );
 compressor.log = CSSCompressor.noop;
 
 
-// RGBA to RGB conversions
-munit( 'Misc.Paren Whitespace', function( assert ) {
-	var rule = CSSCompressor._rulesHash[ 'Paren Whitespace' ].callback;
+// Function Units compression
+munit( 'Misc.Function Units', function( assert ) {
+	var rule = CSSCompressor._rulesHash[ 'Function Units' ].callback;
 
 	[
 
 		{
 			name: 'Basic',
-			actual: 'new-rule( top, 10px, left, 15.5, archaic )',
-			expected: 'new-rule(top,10px,left,15.5,archaic)'
+			actual: 'new-rule( top, 10px, 0.5px, 15.5, foo )',
+			expected: 'new-rule(top,10px,.5px,15.5,foo)'
 		},
 
 		{
 			name: 'Basic Vender Prefix',
-			actual: '-webkit-new-rule( top, 10px, left, 15.5, archaic )',
-			expected: '-webkit-new-rule(top,10px,left,15.5,archaic)'
+			actual: '-webkit-new-rule( top, 10px, left, 15.5, foo )',
+			expected: '-webkit-new-rule(top,10px,left,15.5,foo)'
 		},
 
 		{
 			name: 'Quotes',
-			actual: 'new-rule( top, "10px)", left, \')15.5\', archaic )',
-			expected: 'new-rule(top,"10px)",left,\')15.5\',archaic)'
+			actual: 'new-rule( top, "10px)", left, \')15.5\', foo )',
+			expected: 'new-rule(top,"10px)",left,\')15.5\',foo)'
 		},
 
 		{
 			name: 'Escapes',
-			actual: 'new-rule( top, "10\\"px)", le\\)ft, 15.5, archaic )',
-			expected: 'new-rule(top,"10\\"px)",le\\)ft,15.5,archaic)'
+			actual: 'new-rule( top, "10\\"px)", le\\)ft, 15.5, foo )',
+			expected: 'new-rule(top,"10\\"px)",le\\)ft,15.5,foo)'
 		},
 
 		{
 			name: 'Empty Entry',
 			actual: 'new-rule( top, 10px, left, 15.5, )',
 			expected: 'new-rule(top,10px,left,15.5,)'
+		},
+
+		{
+			name: 'RGB Test',
+			actual: 'new-rule( top, 10px, left, 15.5, rgb( 0, 0, 0 ) )',
+			expected: 'new-rule(top,10px,left,15.5,#000)'
+		},
+
+		{
+			name: 'No Match',
+			actual: '0.5px',
+			expected: undefined
 		}
 
 	].forEach(function( object ) {
