4f242ae97a2467c908cf83d04d84dc68ab6612c6
Corey Hart <corey@codenothing.com>
2013-04-18 09:24:38 -0700

    -Removing longjohn from testing, not useful

--- build/test.js
+++ build/test.js
@@ -3,9 +3,6 @@ global.munit = require( 'munit' );
 global.CSSCompressor = require( '../' );
 global.CSSTree = CSSCompressor.CSSTree;
 
-// Get long stacktraces of errors
-// require( 'longjohn' );
-
 // Only stop test suite when running make test
 if ( ! process.env.NODE_TEST_NO_SKIP ) {
 	munit.defaults.settings.stopOnFail = true;
